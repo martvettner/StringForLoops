f126853d1b90edcfd3b16110bbb4a85be3951b4a
Opilane TTHK <opilane@tthk.ee>
2020-09-21 14:45:12 +0300

    task 2

--- NameLengthComparison/Program.cs
+++ NameLengthComparison/Program.cs
@@ -6,7 +6,23 @@ namespace NameLengthComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter name 1:");
+            string userNameOne = Console.ReadLine();
+
+            Console.WriteLine("Enter name 2:");
+            string userNameTwo = Console.ReadLine();
+
+            int userNameOneLength = userNameOne.Length;
+            int userNameTwoLength = userNameTwo.Length;
+
+            if (userNameOneLength > userNameTwoLength)
+            {
+                Console.WriteLine($"{userNameOne} is longer than {userNameTwo}.");
+            }
+            else
+            {
+                Console.WriteLine($"{userNameTwo} is longer than {userNameOne}.");
+            }
         }
     }
 }
